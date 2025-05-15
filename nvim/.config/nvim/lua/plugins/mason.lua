1bf0a7f1c28b5e76df815b622bb33d9f168c8585
dathuynh-eh <huynhtuandat2797@gmail.com>
2025-05-15 09:58:03 +0700

    chore: update lsp config

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -1,3 +1,31 @@
 return {
   'williamboman/mason.nvim',
+  dependencies = {
+    'williamboman/mason-lspconfig.nvim',
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
+  },
+  config = function()
+    local mason = require 'mason'
+    local mason_tool_installer = require 'mason-tool-installer'
+
+    mason.setup {
+      ui = {
+        icons = {
+          package_installed = '✓',
+          package_pending = '➜',
+          package_uninstalled = '✗',
+        },
+      },
+    }
+
+    mason_tool_installer.setup {
+      ensure_installed = {
+        'lua_ls',
+        'ts_ls',
+        'stylua',
+        'eslint_d',
+        'prettier',
+      },
+    }
+  end,
 }
